f12999829c52e0a1bf8d1bd776dc0512dd8a0f99
Paolo <paodzon@gmail.com>
2024-07-15 17:10:44 +0800

    fix callbacks

--- auth.ts
+++ auth.ts
@@ -33,6 +33,13 @@ export const authOptions: NextAuthOptions = {
     })
   ],
   callbacks: {
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url
+      return baseUrl
+    },
     session: async ({ session, token }) => {
       if (session?.user) {
         if (token.sub) {
